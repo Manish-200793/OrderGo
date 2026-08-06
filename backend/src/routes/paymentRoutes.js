dac7953e913e4b5048ad6b04d88d815cf892d0df
Anirudh8520 <omanirudh2006@gmail.com>
2026-08-06 19:00:33 +0530

    added

--- backend/src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { initiatePayment, getPaymentStatus } = require('../controllers/paymentController');
-const { authenticate } = require('../middleware/auth');
-
-router.post('/initiate', authenticate, initiatePayment);
-router.get('/:orderId', authenticate, getPaymentStatus);
-
-module.exports = router;
